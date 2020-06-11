e24fc6fc2d1f3812b2b5a507c0bc3bb4a96cdb40
jinwoo park <swat018@gmail.com>
2020-06-11 16:01:22 +0900

    Initial commit

---
+++ src/main/java/com/swat018/springbootsecurity2/Springbootsecurity2Application.java
@@ -0,0 +1,13 @@
+package com.swat018.springbootsecurity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootsecurity2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootsecurity2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/swat018/springbootsecurity2/Springbootsecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swat018.springbootsecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootsecurity2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
